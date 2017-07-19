c7bad67ec544a6c7d888fc4a0c43d2617220dc4f
nirmaloasis <nirmaloasis@hotmail.com>
2017-07-19 12:10:32 +0530

    till updating likes

---
+++ client/components/Photo.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Link} from 'react-router'
+import CSSTransactionGroup from "react-addons-css-transition-group";
+
+const Photo = React.createClass({
+    
+
+    render(){
+        return(
+            <figure className="grid-figure">
+              <div className="grid-figure" >
+                <Link to={'/view/${this.props.post.code}'}>
+                  <img src={this.props.post.display_src} alt={this.props.post.caption}/>
+                </Link> 
+                <CSSTransactionGroup
+                    transitionName="like"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500}>
+                    <span key={this.props.post.likes} className="likes-heart">{this.props.post.likes} </span>
+                </CSSTransactionGroup>
+            </div>
+            <figcaption>
+               <p>{this.props.post.caption}</p> 
+               <div className="control-buttons">
+                   <button className="likes" onClick={this.props.increment.bind(null,this.props.i)}>&hearts; {this.props.post.likes}</button>
+                   <Link to={'/view/${this.props.post.code}'}>
+                    <span className="comment-count">
+                        <span className="speech-bubble"></span>
+                        {this.props.comments[this.props.post.code] ?this.props.comments[this.props.post.code].length:0 }
+                    </span>
+                   </Link> 
+               </div>
+          </figcaption>
+        </figure>   
+        )
+    }
+})
+
+export default Photo
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,8 +1,18 @@
 // 1 the action
 // 2 copy of current state
 function posts(state = [],action){
-    console.log("The post will change")
-    console.log(state,action)
-    return state;
+    switch(action.type){
+        case "INCEREMENT_LIKES":
+        console.log("reached")
+          const i = action.index
+            return[
+                ...state.slice(0,i),
+                {...state[i],likes:state[i].likes+1 },
+                ...state.slice(i+1)
+            ]
+        default:
+          return state;
+    }
+
 }
 export default posts
